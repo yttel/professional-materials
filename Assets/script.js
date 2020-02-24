cba142c4d644afd4d7d0f348c3419201a9ef84d3
BytteL <lettybedard@gmail.com>
2020-02-24 18:30:41 -0500

    oops

--- Assets/script.js
+++ Assets/script.js
@@ -1,61 +1,84 @@
-// js for weather dashboard
+// js for professional materials
 // written by Letty Bedard
 
 $(function(){
   
   //  DECLARATIONS
-  let recentList;
+  const details = [
+    {
+      title: "Project 1: DOGS",
+      image: "project1.png",
+      description: "A small group project where were tasked with designing our own site. We decided to make one that would give dog owners the weather and dog friendly businesses for the location they entered.",
+      gitLink: "https://yttel.github.io/DOGS/",
+      gitSite: "https://github.com/yttel/DOGS"
+    },{
+      title: "JavaScript Coding Quiz",
+      image: "quiz.png",
+      description: "Basic quiz with saved high scores and a timer to see how you stack up on basic JavaScript.",
+      gitLink: "https://yttel.github.io/javascript-quiz/",
+      gitSite: "https://github.com/yttel/javascript-quiz"
+    },{
+      title: "Workday Scheduler",
+      image: "workday.png",
+      description: "Workday scheduler that allows the user to enter their tasks for the day. Saved even when refreshed and updates the colors of the time slots based on the current time.",
+      siteLink: "https://yttel.github.io/workday-scheduler/",
+      gitLink: "https://github.com/yttel/workday-scheduler"
+    },{
+      title: "Weather Dashboard",
+      image: "weather.png",
+      description: "User enters the location they wish to search, yielding the current weather and 5 day forecast. Recent places searched is saved on the sidebar, and are clickable so they can be revisited later.",
+      siteLink: "https://yttel.github.io/weather-dashboard/",
+      gitLink: "https://github.com/yttel/weather-dashboard"
+    }];
 
   //  REFERENCES
-  let $aboutMe = $("#aboutMe");
-  let $portfolio = $("#portfolio");
-  let $cards = [
-    $("#card1"), 
-    $("#card2"), 
-    $("#card3"), 
-    $("#card4"), 
-    $("#card5")];
 
   //  FUNCTIONS
+
+  //dump info from details[index] into the place
+  const drawCard = (index) => {
+    let $cardDetail = $("#cardDetail");
+    const {title, image, description, siteLink, gitLink} = details[index];
+    $cardDetail.empty();
+    $cardDetail.html(`
+    <div class="row">
+      <div class="col-md-6">
+        <img class="w-100" src="./Assets/Images/${image}">
+      </div>
+
+      <div class="col-md-6">
+        <h3 class="px-1">${title}</h3>
+        <a class="px-1" href="${siteLink}" target="_blank">Link to Live site</a><br>
+        <a class="px-1 "href="${gitLink}" target="_blank">Link to GitHub</a>
+        <p class="px-1">${description}</p>
+      </div>
+    </div>`);
+  }
+
+  const hideAll = () => {
+    $("#mainAttraction").children().each(function() {
+        $(this).addClass("isHidden");
+    });
+  };
+
+  //hide all panes, show #paneTitle
+  const showPane = (paneTitle) => {
+    hideAll();
+    $(`#${paneTitle}`).removeClass("isHidden");
+  };
+
     
   //  EVENT LISTENERS
+
+  $(document).on("click", "#aboutMe", function(){
+    showPane("aboutMePane");
+  });  
+  $(document).on("click", "#portfolio", function(){
+    showPane("portfolioPane");
+  });  
   
-  //search button is pushed, add search term to top of recent list and show this place
-  $(document).on("click", "#srchBtn", function(){
-    recentList = JSON.parse(localStorage.getItem("recentList"));
-    if (recentList === null){
-      recentList = [];
-    }
-    let thisInput = $search.val().trim();
-    //assume good input for now
-    if ($.isNumeric(thisInput)){
-      recentList.unshift({name: null, zip: thisInput});
-    }
-    else {
-      recentList.unshift({name: thisInput, zip: null});
-    }
-    localStorage.setItem("recentList", JSON.stringify(recentList));
-    renderAll();
+  //when card is clicked, redraw detail pane with info from details array of objects
+  $(document).on("click", ".card", function(){
+    drawCard($(this).attr("data-index"));
   });
-
-  //when clear recent is clicked, empty that div and clear local
-  $(document).on("click", "#reset", function(){
-    localStorage.removeItem("recentList");
-    renderAll();
-  });
-
-  //when one of the recent locations is clicked show that weather
-  $(document).on("click", ".recentPlace", function(){
-    $current.empty()
-    let goHere = $(this).attr("data-place");
-    if ($.isNumeric(goHere)){
-      getWeatherByZip(goHere);
-    }
-    else {
-      getWeatherByName(goHere);
-    }
-  });
-
-renderAll();
-  
 });
